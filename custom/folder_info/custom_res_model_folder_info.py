a26b0bea3f66bd05f91c146079c3cff2d8a31919
your_username <your_email@example.com>
2024-11-22 22:24:53 +0800

    表格信息绑定技能

--- custom/folder_info/custom_res_model_folder_info.py
+++ custom/folder_info/custom_res_model_folder_info.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'RGmy8oh2Ee+GlV9pvwI8Pi', '建筑')], 'd': {}}
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 0, 'wmPSLZscEe+DCMmKPPrTj+', '武器头像'), ('/2147483647', 1, 'RGmy8oh2Ee+GlV9pvwI8Pi', '建筑')], 'd': {}}
