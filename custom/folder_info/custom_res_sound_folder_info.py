8805d0b3ecd1621ed1b8333a49d3e7e512c2d71c
your_username <your_email@example.com>
2024-12-19 16:57:41 +0800

    new

--- custom/folder_info/custom_res_sound_folder_info.py
+++ custom/folder_info/custom_res_sound_folder_info.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name')], 'd': {}}
+data = {'f': [('', 0, 2147483647, 'code_explorer_custom_root_folder_name'), ('/2147483647', 1, 'SpOGGLk0Ee+8wqkvadHv2t', '音效')], 'd': {'134222749': (2147483647, 0), '134244220': ('SpOGGLk0Ee+8wqkvadHv2t', 0), '134257274': ('SpOGGLk0Ee+8wqkvadHv2t', 1), '134261867': ('SpOGGLk0Ee+8wqkvadHv2t', 2), '134263209': ('SpOGGLk0Ee+8wqkvadHv2t', 3)}}
